dc8140190cab2ce8133f037f1d2a25252b677a80
Eddie Eldridge <steadyeddie101@hotmail.co.uk>
2022-12-08 22:39:29 +0000

    Load DDS textures instead of PNG and add the ability to select models from a dropdown

--- static/js/model.js
+++ static/js/model.js
@@ -1,66 +1,60 @@
-var container, controls, camera, scene, renderer;
-var windowHalfX = window.innerWidth / 2;
-var windowHalfY = window.innerHeight / 2;
-var clock = new THREE.Clock();
+let windowHalfX = window.innerWidth / 2;
+let windowHalfY = window.innerHeight / 2;
+let controls;
+
+const ddsLoader = new THREE.DDSLoader();
+const clock = new THREE.Clock();
+const manager = new THREE.LoadingManager();
+const textureLoader = new THREE.TextureLoader();
+const scene = new THREE.Scene();
+const renderer = new THREE.WebGLRenderer({ alpha: true });
+const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.01, 1000000);
+const loader = new THREE.ColladaLoader(manager);
+const debug = true;
+
+let dae;
+let dae_path = "";
+let mainDDSPath = "";
+let attachDDSPath = ""
 
 init();
 
-async function init() {
-  await new Promise(r => setTimeout(r, 2000));
+// Set new dae, main texture and attachement texture paths
+function setModelPaths(model) {
+  switch (model) {
+    case "Orc Host":
+      dae_path = "/models/orcs/AGO_BetterOrcs.dae";
+      mainDDSPath = "/models/orcs/ago_orcs.dds";
+      attachDDSPath = "/models/orcs/ago_orc_att.dds";
+      break;
 
-  // Setup the DOM elements
-  container = document.createElement('div');
-  contentBody = document.getElementById('content-body');
-  container.className = "canvas-container"
-  contentBody.appendChild(container);
+    case "Ruffians":
+      dae_path = "/models/rebels/AGO_BetterOrcs.dae";
+      mainDDSPath = "test";
+      attachDDSPath = "test";
 
-  // Dropdown Event Listener
-  const activities = document.getElementById('background-select');
+      break;
+    case "Wulfingir":
+      dae_path = "/models/rebels/AGO_BetterOrcs.dae";
+      mainDDSPath = "/models/rebels/ago_orcs.dds";
+      attachDDSPath = "/models/rebels/ago_orc_att.dds";
+      break;
 
-  activities.addEventListener('change', (e) => {
-    console.log(`e.target.value = ${e.target.value}`);
-    console.log(`activities.options[activities.selectedIndex].value = ${activities.options[activities.selectedIndex].value}`);
-    setBackgroundImage(e.target.value);
-  });
+    case "Armsmen":
+      dae_path = "/models/rebels/AGO_BetterOrcs.dae";
+      mainDDSPath = "/models/rebels/ago_orcs.dds";
+      attachDDSPath = "/models/rebels/ago_orc_att.dds";
+      break;
 
-  // Create the camera and position it
-  camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.01, 1000000);
-  camera.position.set(1, 1, -1);
+    default:
+      dae_path = "/models/orcs/AGO_BetterOrcs.dae";
+      mainDDSPath = "/models/orcs/ago_orcs.dds";
+      attachDDSPath = "/models/orcs/ago_orc_att.dds";
+      break;
+  }
+}
 
-  // Create the scene and add some ambient lights
-  scene = new THREE.Scene();
-  // var ambientLight = new THREE.AmbientLight( 0xcccccc, 0.4 );
-  // var pointLight = new THREE.PointLight( 0xffffff, 0.6 );
-
-  const ambientLight = new THREE.AmbientLight(0xcccccc, 1);
-  scene.add(ambientLight);
-
-  const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
-  directionalLight.position.set(1, 1, 0).normalize();
-
-  scene.add(directionalLight);
-  camera.add(ambientLight);
-  camera.add(directionalLight);
-  scene.add(camera);
-
-  // Create our WebGL Renderer
-  renderer = new THREE.WebGLRenderer({ alpha: true });
-  renderer.setPixelRatio(10);
-  // renderer.setClearColor(0x000000, 0);
-  // renderer.setSize(window.innerWidth, window.innerHeight);
-  // renderer.setClearColor("rgb(34, 34, 34)");
-  renderer.gammaFactor = 1.5;
-  renderer.outputEncoding = THREE.sRGBEncoding;
-  // Add the background
-  const loader = new THREE.TextureLoader();
-  setBackgroundImage();
-
-  // loader.load('https://i.imgur.com/SIFCW0f.jpg', function (texture) {
-  //   texture.encoding = THREE.sRGBEncoding;
-  //   scene.background = texture;
-  // });
-
-
+// Change the background image
 function setBackgroundImage(image) {
   let backgroundUrl = ""
 
@@ -74,121 +68,186 @@ function setBackgroundImage(image) {
     case "Dunland":
       backgroundUrl = "https://i.imgur.com/P4QsUbx.jpg"
       break;
+    case "Minas Tirith":
+      backgroundUrl = "https://i.imgur.com/R3HJpyb.png"
+      break;
     default:
       backgroundUrl = "https://i.imgur.com/SIFCW0f.jpg"
       break;
   }
 
-  loader.load(backgroundUrl, function (texture) {
+  textureLoader.load(backgroundUrl, function (texture) {
     texture.encoding = THREE.sRGBEncoding;
     scene.background = texture;
   });
 }
+
+async function init() {
+  await new Promise(r => setTimeout(r, 2000));
+  let container;
+
+  // Setup the DOM elements
+  container = document.createElement('div');
+  contentBody = document.getElementById('content-body');
+  container.className = "canvas-container"
+  contentBody.appendChild(container);
+
+  // Setup our Dropdown Event Listeners
+  const backgroundSelect = document.getElementById('background-select');
+  const modelSelect = document.getElementById('model-select');
+
+  backgroundSelect.addEventListener('change', (e) => {
+    console.log(`e.target.value = ${e.target.value}`);
+    setBackgroundImage(e.target.value);
+  });
+
+  modelSelect.addEventListener('change', (e) => {
+    console.log(`e.target.value = ${e.target.value}`);
+    loadModel(e.target.value, true)
+  });
+
+  // Create the camera and position it
+  camera.position.set(-0.00632805627663537, 1.5, -2);
+
+  // Add some ambient lights
+  const ambientLight = new THREE.AmbientLight(0xcccccc, 1);
+  scene.add(ambientLight);
+  camera.add(ambientLight);
+  scene.add(camera);
+
+  // Create our WebGL Renderer
+  renderer.setPixelRatio(10);
+  renderer.gammaFactor = 1.5;
+  renderer.outputEncoding = THREE.sRGBEncoding;
+
+  // Add the background
+  setBackgroundImage("Mordor");
 
   container.appendChild(renderer.domElement);
 
   // Add some basic controls
   controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-  // Setup our resize event listener
-  window.addEventListener('resize', onWindowResize, false);
-  window.addEventListener("keydown", onKeyDown);
-
-  // Choose files handler
-  document.querySelector('.inputfile').addEventListener('change', function (e) {
-    var files = e.currentTarget.files;
-    var dae_path;
-
-    var extraFiles = {}, file;
-    for (var i = 0; i < files.length; i++) {
-      file = files[i];
-      extraFiles[file.name] = file;
-
-      //Filename ends in .dae/.DAE
-      if (files[i].name.match(/\w*.dae\b/i)) {
-        dae_path = files[i].name;
-      }
-    }
-
-    const manager = new THREE.LoadingManager();
-    manager.setURLModifier(function (url, path) {
-      url = url.split('/');
-      url = url[url.length - 1];
-
-      if (extraFiles[url] !== undefined) {
-
-        var blobURL = URL.createObjectURL(extraFiles[url]);
-        console.log(blobURL); //Blob location created from files selected from file input
-        return blobURL;
-
-      }
-      return url;
-    });
-
-    // Loud our Collada model
-    var loader = new THREE.ColladaLoader(manager);
-    loader.load(dae_path, function (collada) {
-
-      console.info(collada);
-      var dae = collada.scene;
-      mesh = scene.children[0].children[0].clone();
-
-      console.log('Mesh')
-      console.log(mesh)
-      // console.log(animations.scene)
-
-      collada.scene.traverse(function (child) {
-        if (child.material) {
-          // child.material.opacity = 0.9;
-          child.material.transparent = true;
-          // child.material.premultipliedAlpha = true;
-          child.material.alphaTest = 0.5;
-        }
-      });
-
-      scene.add(dae);
-      // scene.background = null;
-
-
-      // console.log(collada);
-      // var dae = collada.scene;
-      // scene.add(dae);
-
-    });
-  });
-
-  animate();
-
-}
-
-function onKeyDown(e) {
-  console.log(e);
-
-  if (e.code = 'Space') {
-    console.log('SPACE!');
-    animate();
-  }
-}
-
-function onWindowResize() {
-
-  // windowHalfX = window.innerWidth / 2;
-  // windowHalfY = window.innerHeight / 2;
-  // camera.aspect = window.innerWidth / window.innerHeight;
-  // camera.updateProjectionMatrix();
-  // renderer.setSize( window.innerWidth, window.innerHeight );
-
-}
+  loadModel("Orcs", false)
+};
 
 async function animate() {
   requestAnimationFrame(animate);
   var delta = clock.getDelta();
   controls.update(delta);
   render();
-
 }
 
 async function render() {
   renderer.render(scene, camera);
 }
 
+async function loadModel(model, shouldReset) {
+  setModelPaths(model);
+
+  // If we call this function but there's already a model present, we can optionally reset
+  // the scene and tidy things up
+  if (shouldReset) {
+    console.log('Removing model...');
+    disposeNode(dae)
+    scene.remove(dae);
+  }
+
+  // Loud our Collada model
+  loader.load(dae_path, function (collada) {
+    console.log('Loading model...');
+
+    dae = collada.scene;
+
+    // Setup transparency for the Alpha layer on our textures
+    mesh = scene.children[0].children[0].clone();
+
+    collada.scene.traverse(function (child) {
+      if (child.material && child.isMesh) {
+        child.material.transparent = true;
+        child.material.alphaTest = 0.5;
+      }
+    });
+
+    // Loud our two textures (main and attach)
+    const mainTexture = ddsLoader.load(mainDDSPath);
+    const attachTexture = ddsLoader.load(attachDDSPath);
+
+    scene.add(dae);
+
+    // Debug Print
+    if (debug == true) {
+      console.info(collada);
+      console.info(mainTexture);
+      console.info(attachTexture);
+      console.info(dae_path);
+      console.info(mainDDSPath);
+      console.info(attachDDSPath);
+      console.info(renderer);
+      console.info(scene);
+      console.info(camera);
+    }
+    animate();
+  });
+}
+
+
+// Dispose Nodes to free up memory
+function disposeNode(dae) {
+  dae.traverse(function (node) {
+    if (node instanceof THREE.Mesh) {
+      if (node.geometry) {
+        node.geometry.dispose();
+      }
+      if (node.material) {
+        var materialArray;
+        if (node.material) {
+          materialArray = node.material.materials;
+        }
+        else if (node.material instanceof Array) {
+          materialArray = node.material;
+        }
+        if (materialArray) {
+          materialArray.forEach(function (mtrl, idx) {
+            if (mtrl.map) mtrl.map.dispose();
+            if (mtrl.lightMap) mtrl.lightMap.dispose();
+            if (mtrl.bumpMap) mtrl.bumpMap.dispose();
+            if (mtrl.normalMap) mtrl.normalMap.dispose();
+            if (mtrl.specularMap) mtrl.specularMap.dispose();
+            if (mtrl.envMap) mtrl.envMap.dispose();
+            mtrl.dispose();
+          });
+        }
+        else {
+          if (node.material.map) node.material.map.dispose();
+          if (node.material.lightMap) node.material.lightMap.dispose();
+          if (node.material.bumpMap) node.material.bumpMap.dispose();
+          if (node.material.normalMap) node.material.normalMap.dispose();
+          if (node.material.specularMap) node.material.specularMap.dispose();
+          if (node.material.envMap) node.material.envMap.dispose();
+          node.material.dispose();
+        }
+      }
+    }
+  });
+}
+
+// function onKeyDown(e) {
+//   console.log(e);
+
+//   if (e.code = 'Space') {
+//     console.log('SPACE!');
+//     animate();
+//   }
+// }
+
+
+// function onWindowResize() {
+
+//   // windowHalfX = window.innerWidth / 2;
+//   // windowHalfY = window.innerHeight / 2;
+//   // camera.aspect = window.innerWidth / window.innerHeight;
+//   // camera.updateProjectionMatrix();
+//   // renderer.setSize( window.innerWidth, window.innerHeight );
+
+// }
